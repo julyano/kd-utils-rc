23d6e29dd441c132a2d495f0d703a738ca98f355
juliano <ju.ca.mendonca@hotmail.com>
2022-02-23 21:27:02 -0300

    make dist folder

---
+++ dist/formatter.d.ts
@@ -0,0 +1,1 @@
+export declare function formatCep(value: string): string;
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.formatCep = void 0;
+var formatter_1 = require("./formatter");
+Object.defineProperty(exports, "formatCep", { enumerable: true, get: function () { return formatter_1.formatCep; } });
